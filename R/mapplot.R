134d4380f7286baec84e7d4ec9219963a218d059
GuangchuangYu <guangchuangyu@gmail.com>
2017-11-24 00:08:37 +0800

    pass R check

--- R/mapplot.R
+++ R/mapplot.R
@@ -1,5 +1,37 @@
+##' Enrichment Map 
+##'
+##' 
+##' @title mapplot
+##' @param x enrichment result. e.g. instance of gseaResult or enrichResult
+##' @param showCategory number of enriched terms to display
+##' @param color variable that used to color enriched terms, e.g. pvalue, p.adjust or qvalue
+##' @param layout layout of the map
+##' @param ... additional parameter
+##' @return ggplot object
+##' @importFrom DOSE geneInCategory
+##' @importFrom igraph graph.empty
+##' @importFrom igraph add_vertices
+##' @importFrom igraph graph.data.frame
+##' @importFrom igraph delete.edges
+##' @importFrom igraph V "V<-"
+##' @importFrom igraph E "E<-"
+##' @importFrom reshape2 melt
+##' @importFrom ggplot2 aes_
+##' @importFrom ggplot2 scale_color_gradientn
+##' @importFrom ggplot2 guide_colorbar
+##' @importFrom ggplot2 scale_size
+##' @importFrom ggplot2 theme_void
+##' @importFrom ggraph ggraph
+##' @importFrom ggraph geom_node_point
+##' @importFrom ggraph geom_node_text
+##' @importFrom ggraph geom_edge_link
+##' @export
+##' @author Guangchuang Yu 
+mapplot <- function(x, showCategory = 50, color="p.adjust", layout = "kk", ...) {
+    if (!inherits(x, "gseaResult") && !inherits(x, "enrichResult"))
+        stop("object not supported...")
 
-mapplot <- function(x, n = 50, layout = "kk", color="p.adjust", ...) {
+    n <- showCategory
     geneSets <- geneInCategory(x) ## use core gene for gsea result
     y <- as.data.frame(x)
     if (nrow(y) < n) {
@@ -54,7 +86,9 @@ mapplot <- function(x, n = 50, layout = "kk", color="p.adjust", ...) {
 
 }
 
-heatmap_palette <- palette <- colorRampPalette(c("red", "yellow", "green"))(n = 299)
+
+##' @importFrom grDevices colorRampPalette
+heatmap_palette <- colorRampPalette(c("red", "yellow", "green"))(n = 299)
 
 
 overlap_ratio <- function(x, y) {
