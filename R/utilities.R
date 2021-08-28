3ea809a383ce37bdab78ba05c2fa766cf8e39b2e
huerqiang <13766876214@163.com>
2021-08-28 15:52:13 +0800

    fix a word

--- R/utilities.R
+++ R/utilities.R
@@ -351,7 +351,7 @@ ep_str_wrap <- function(string, width) {
                    # When the length of first word > width
                    if (length(idx) == 0) idx <- 1
                    # Split the string into two pieces
-                   # The length of first piece is small than witdh
+                   # The length of first piece is small than width
                    words[[i]] <- substring(st, 1, y[idx[length(idx)]] - 1)
                    st <- substring(st, y[idx[length(idx)]] + 1, n)  
                    i <- i + 1
